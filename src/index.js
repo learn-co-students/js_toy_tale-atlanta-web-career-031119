8c19e082127f386b4c2f41bd7f5785a211103391
Darcy Linde <lindedarcy@gmail.com>
2019-04-26 11:54:21 -0400

    all deliveables working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const submitBtn = document.querySelector('.submit')
 let addToy = false
 
-// YOUR CODE HERE
+fetchToys() // STEP 2 && 3: Fetch initial toys and create full cards
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +17,83 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+submitBtn.addEventListener('click', () => {
+  let inputs = document.querySelectorAll('.input-text')
+  createToy(inputs[0].value, inputs[1].value);
+})
 
-// OR HERE!
+function fetchToys(){
+    fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(data => data.forEach(toy => handleToy(toy)))
+}
+
+function handleToy(toy){
+  let parent = document.querySelector('#toy-collection')
+
+    let div = document.createElement('div')
+    div.className = 'card'
+
+    let h2 = document.createElement('h2')
+    h2.innerText = toy.name
+
+    let img = document.createElement('img')
+    img.src = toy.image
+    img.className = 'toy-avatar'
+
+    let p = document.createElement('p')
+    p.innerText = `${toy.likes} likes`
+    p.id = `toy-${toy.id}`
+
+    let button = document.createElement('button')
+    button.className = 'like-btn'
+    button.innerText = 'Like <3'
+    button.dataset.id = toy.id
+    button.dataset.likes = toy.likes
+    button.addEventListener('click', () => {
+      increaseLikes(button.dataset.id, button.dataset.likes);
+    })
+
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+    div.appendChild(button)
+    parent.appendChild(div)
+}
+
+function createToy(name, image){
+  fetch('http://localhost:3000/toys', {
+        method: 'POST',
+        headers:    {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          image: image,
+          likes: 0
+        })
+    })
+}
+
+function increaseLikes(id, likes){
+  let newLikes = Number(likes);
+  newLikes += 1;
+  fetch(`http://localhost:3000/toys/${id}`, {
+    method: 'PATCH',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+
+    body: JSON.stringify({
+      likes: newLikes
+    })
+  })
+  editDom(id, newLikes);
+}
+
+function editDom(id, newLikes){
+  let p = document.querySelector(`#toy-${id}`)
+  p.innerText = `${newLikes} likes`
+}
